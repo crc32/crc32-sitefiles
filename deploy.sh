e23dff1bf473c9976babd84d9128b6209d56b0d2
Colin Crossman <colin@distributedha.sh>
2022-10-26 12:11:28 -0600

    Update

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,15 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Remove current public folder
+rm -fR public
+mkdir public
+
 # Build the project.
 hugo -t researcher # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
+cp -R ./public/ /Users/crc32/Projects/crc32.github.io/
+cd /Users/crc32/Projects/crc32.github.io/
 
 # Add changes to git.
 git add .
